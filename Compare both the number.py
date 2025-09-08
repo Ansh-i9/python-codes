e2577780cbdda3b83e91dbc29f6743226669def3
ANSH_RAVAL <ansh132011@gmail.com>
2025-09-08 22:20:17 +0530

    Add files via upload

---
+++ Compare both the number.py
@@ -0,0 +1,7 @@
+N1 = int(input("N1: "))      
+N2 = int(input("N2: ")) 
+#Compare both the number 
+ if N1 >= N2:
+    print("N1 is greater than or equal to N2.")       
+    else:  
+     print("N2 is smaller than N1.")
